ff6e0e1d1f3a600cb35be952cbd85038e3fe9163
hervesson <hervessonporto@hotmail.com>
2021-04-12 20:34:18 -0300

    Criação do arquivo initialize Firebase

---
+++ src/Services/FirebaseConfig.js
@@ -0,0 +1,22 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/database"
+import "firebase/storage"
+
+
+var firebaseConfig = {
+    apiKey: "AIzaSyBdpJGysnIanm0U8CXenIVH5Zm0q5e_S1o",
+  	authDomain: "myappdonate.firebaseapp.com",
+  	projectId: "myappdonate",
+  	storageBucket: "myappdonate.appspot.com",
+  	messagingSenderId: "197195621436",
+  	appId: "1:197195621436:web:37f4a7f1374ddff3030f39",
+  	measurementId: "G-09456FRDLP"
+};
+
+
+firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+export const database = firebase.database();
+export const storage = firebase.storage();
