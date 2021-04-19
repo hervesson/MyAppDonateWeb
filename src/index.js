c2c76d810870164b4c5edafe462d48ab9ced0a38
hervesson <hervessonporto@hotmail.com>
2021-04-19 18:32:33 -0300

    Provendo a store para a aplicação

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import { Provider } from 'react-redux';
+import { configureStore } from './Redux/store';
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  	<Provider store={configureStore()}>
+    	<App />
+    </Provider>,	
   document.getElementById('root')
 );
 
